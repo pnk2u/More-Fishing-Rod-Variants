7c46a7d028a1c352a409004c2a4714df64dbca3c
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-24 17:03:42 +0100

    move 1.21.3 item declaration back to def branch
    useful for compatibility (tide) and readability
    + increment version number: 1.0.4

--- src/main/java/de/pnku/mstv_mfrv/MoreFishingRodVariants.java
+++ src/main/java/de/pnku/mstv_mfrv/MoreFishingRodVariants.java
@@ -4,6 +4,7 @@ import de.pnku.mstv_base.item.MoreStickVariantItems;
 import de.pnku.mstv_mfrv.item.MoreFishingRodVariantItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +14,14 @@ public class MoreFishingRodVariants implements ModInitializer {
 
 	public static final String MOD_ID = "mstv-mfrv";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static boolean isTideLoaded = false;
 
 	
 	@Override
 	public void onInitialize() {
+		if(FabricLoader.getInstance().isModLoaded("tide")) {
+			isTideLoaded = true;
+		}
 		MoreFishingRodVariantItems.registerRodItems();
 	}
 
