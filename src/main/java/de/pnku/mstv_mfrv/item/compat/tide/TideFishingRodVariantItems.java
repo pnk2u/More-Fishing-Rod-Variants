5e51e95a8da96eca2d2b4b6372a718120a876851
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-26 20:42:42 +0100

    recipe format (1.21.3) and comment out tide compat

--- src/main/java/de/pnku/mstv_mfrv/item/compat/tide/TideFishingRodVariantItems.java
+++ src/main/java/de/pnku/mstv_mfrv/item/compat/tide/TideFishingRodVariantItems.java
@@ -1,6 +1,6 @@
 package de.pnku.mstv_mfrv.item.compat.tide;
 
-import com.li64.tide.registries.items.TideFishingRodItem;
+//import com.li64.tide.registries.items.TideFishingRodItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -11,15 +11,17 @@ public class TideFishingRodVariantItems {
     // Tide Compatibility
 
     public static Item createTideFishingRodItem(String woodType) {
-        Item.Properties tideRodProperties = new Item.Properties().durability(20);
-        if (woodType.matches("crimson|warped")) {
-            tideRodProperties.fireResistant();
-        }
-        LOGGER.info("Variant of Tide Fishing Rod registered."); // DEBUG, to be removed
-        return new TideFishingRodItem(32, tideRodProperties);
+//        Item.Properties tideRodProperties = new Item.Properties().durability(20);
+//        if (woodType.matches("crimson|warped")) {
+//            tideRodProperties.fireResistant();
+//        }
+//        LOGGER.info("Variant of Tide Fishing Rod registered."); // DEBUG, to be removed
+//        return new TideFishingRodItem(32, tideRodProperties);
+        return null;
     }
 
     public static boolean stackIsTide(ItemStack itemStack) {
-        return itemStack.getItem() instanceof TideFishingRodItem;
+//        return itemStack.getItem() instanceof TideFishingRodItem;
+        return false;
     }
 }
